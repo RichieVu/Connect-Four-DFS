c4c59478bc9113e08242d4961e03f3221956137a
VuChu <richievu6693@gmail.com>
2020-12-06 20:32:24 -0800

    Updated main

--- EC-CS-A250/EC-CS-A250/Project1/Main.cpp
+++ EC-CS-A250/EC-CS-A250/Project1/Main.cpp
@@ -23,25 +23,31 @@ int main()
 	list.push(1);
 	list.push(2);
 	list.push(3);
+	cout << "Intial List\n";
 	cout << list << endl;
 
-	cout << list.pop() << endl;
-	cout << list << endl;
+	cout << "Element to Remove" << list.pop() << endl << endl;
 
-	cout << list.find(1) << list.find(2) << list.find(3) << list.find(4) << endl;
+	cout << "Removed Element List" << list << endl << endl;
 
+	cout << list.find(1) << list.find(2) << list.find(3) << list.find(4) << endl << endl;
+
+
+	cout << "Create Copy\n";
 	SLL<int> copy;
-	copy = list;
-	cout << copy << endl;
+	copy = list; // Overloaded assignment operator test
+	cout << copy << endl << endl;
+
+
+	cout << "Copy of Copy\n";
+	SLL<int> copy2(copy); // Copy constructor test
+	cout << copy2 << endl << endl;
+
+	cout << "Printing Deleted List\n";
 	copy.destroyList();
+	cout << copy << endl;
 
-	SLL<int> copy2(list);
-	copy2.push(6);
-	cout << copy2 << endl;
-
-	SLL<int> copy2(copy);
-	cout << copy2 << endl;
-
+	cout << "List of Strings\n";
 	SLL<string> stringList;
 	stringList.push("C++");
 	stringList.push("Java");
